29b50b3f5a9721775015187706ff6ad6124cd60a
Administrator <Administrator@PC-20200722CRUR>
2021-03-28 21:32:26 +0800

    添加项目文件。

---
+++ StructureAlgorithm/AlgorithmTool/BasicSearchTool.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureAlgorithm.AlgorithmTool
+{
+    public static class BasicSearchTool
+    {
+        public static void Show()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random(i + DateTime.Now.Millisecond).Next(0, 100);
+            }
+            //array.Show();
+            //int iResult = -1;
+            //Console.WriteLine("find your int number");
+            //while (iResult < 0)
+            //{
+            //    Console.WriteLine("please input your int number");
+            //    string sValue = Console.ReadLine();
+            //    if (!int.TryParse(sValue, out int iVaule))
+            //    {
+            //        Console.WriteLine("please input right number");
+            //    }
+            //    else
+            //    {
+            //        //iResult = array.SequentialSearch(iVaule);
+            //        bool bResult = array.SequentialSearchWithSelfOrganizing(iVaule);
+            //    }
+            //}
+            array.InsertionSort();
+            array.Show();
+            Console.WriteLine("please input your int number");
+            string sValue = Console.ReadLine();
+            int.TryParse(sValue, out int iVaule);
+            array.BinarySearch(iVaule);
+            //Array.BinarySearch(array, 123);//内置查找
+        }
+
+        /// <summary>
+        /// 顺序查找
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="iValue"></param>
+        /// <returns>所在索引</returns>
+        public static int SequentialSearch(this int[] arr, int iValue)
+        {
+            for (int index = 0; index < arr.Length; index++)
+            {
+                if (arr[index] == iValue)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+
+        public static int Min(this int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+        public static int Max(this int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        #region 自组织查找
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="sValue"></param>
+        /// <returns></returns>
+        public static bool SequentialSearchWithSelfOrganizing(this int[] arr, int sValue)
+        {
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                if (arr[index] == sValue)
+                {
+                    if (index > 0)
+                    {
+                        int temp = arr[index - 1];
+                        arr[index - 1] = arr[index];
+                        arr[index] = temp;
+                        arr.Show();
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 28原则优化
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="sValue"></param>
+        /// <returns></returns>
+        public static int SequentialSearchWithSelfOrganizing28(this int[] arr, int sValue)
+        {
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                if (arr[index] == sValue)
+                {
+                    if (index > (arr.Length * 0.2))
+                    {
+                        if (index > 0)
+                        {
+                            int temp = arr[index - 1];
+                            arr[index - 1] = arr[index];
+                            arr[index] = temp;
+                            arr.Show();
+                        }
+                    }
+                    return index;
+                }
+            }
+            return -1;
+        }
+        #endregion
+
+        #region 二叉查找
+        /// <summary>
+        /// 二叉查找
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int BinarySearch(this int[] arr, int value)
+        {
+            int right = arr.Length - 1;
+            int left = 0;
+            int middle;
+            while (left <= right)
+            {
+                middle = (right + left) / 2;
+                if (arr[middle] == value)
+                {
+                    return middle;
+                }
+                else if (value < arr[middle])
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 递归式二叉查找
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static int BinarySearchRecursion(this int[] arr, int value, int left, int right)
+        {
+            if (left > right)
+            {
+                return -1;
+            }
+            else
+            {
+                int middle = (int)(right + left) / 2;
+                if (value < arr[middle])
+                {
+                    return arr.BinarySearchRecursion(value, left, middle - 1);
+                }
+                else if (value == arr[middle])
+                {
+                    return middle;
+                }
+                else
+                {
+                    return arr.BinarySearchRecursion(value, middle + 1, right);
+                }
+            }
+        }
+        #endregion
+
+        private static void Show(this int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item.ToString("00") + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StructureAlgorithm/AlgorithmTool/BasicSortTool.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureAlgorithm.AlgorithmTool
+{
+    public static class BasicSortTool
+    {
+        public static void Show()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random(i + DateTime.Now.Millisecond).Next(100, 999);
+            }
+
+            Console.WriteLine("before BubbleSort");
+            array.Show();
+            Console.WriteLine("start BubbleSort");
+            //array.BubbleSort();
+            //array.SelectionSort();
+            array.InsertionSort();
+
+            Console.WriteLine("  end BubbleSort");
+            array.Show();
+        }
+
+
+        /// <summary>
+        /// 冒泡排序
+        /// 先挑最大值 摆在最后面
+        /// 先挑最小值 摆在最前面？
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void BubbleSort(this int[] arr)
+        {
+            int temp;
+            for (int outer = arr.Length; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (arr[inner] > arr[inner + 1])
+                    {
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+                }
+                arr.Show();
+            }
+        }
+        /// <summary>
+        /// 选择排序
+        /// 依次选择最小的数字放到最左边
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void SelectionSort(this int[] arr)
+        {
+            int min, temp;
+            for (int outer = 0; outer < arr.Length; outer++)
+            {
+                min = outer;
+                for (int inner = outer + 1; inner < arr.Length; inner++)
+                {
+                    if (arr[inner] < arr[min])
+                    {
+                        min = inner;
+                    }
+                }
+                temp = arr[outer];
+                arr[outer] = arr[min];
+                arr[min] = temp;
+                arr.Show();
+            }
+        }
+
+        /// <summary>
+        /// 插入排序
+        /// 从第2个数开始，跟第一个数对比，放在左边还是右边
+        /// 循环下去比较，都放在合适的位置
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void InsertionSort(this int[] arr)
+        {
+            int inner, temp;
+            for (int outer = 1; outer < arr.Length; outer++)
+            {
+                temp = arr[outer];
+                inner = outer;
+                while (inner > 0 && arr[inner - 1] >= temp)
+                {
+                    arr[inner] = arr[inner - 1];
+                    inner -= 1;
+                }
+                arr[inner] = temp;
+                arr.Show();
+            }
+        }
+
+        private static void Show(this int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
